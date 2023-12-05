18d950b85fa65e3f89742ec235c531385fe8e0fc
P1nkG00s3 <147384574+P1nkG00s3@users.noreply.github.com>
2023-12-06 02:18:19 +0300

    js anim for menu

--- scripts/menu_button.js
+++ scripts/menu_button.js
@@ -4,10 +4,13 @@ function cgh(id){
         button.src = '/images/IsActive_menu.png';
         let elem = document.getElementById("nav-list");
         elem.style.display = 'block';
+        elem.style.animation = 'move_on 0.15s linear';
+        elem.style.opacity = '1';
     }
     else{
         button.src='/images/icon%20menu.png';
         let elem = document.getElementById("nav-list");
-        elem.style.display = 'none';
+        elem.style.animation = 'move_back 0.15s linear';
+        elem.style.opacity = '0';
     }
 }
