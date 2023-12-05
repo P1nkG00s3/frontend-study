d52009fde358e38cba62ef8d3b9192b89b6a91d3
P1nk_G00se <pinkgoose03@gmaill.com>
2023-12-05 17:04:48 +0300

    upgrade js code

--- scripts/menu_button.js
+++ scripts/menu_button.js
@@ -1,9 +1,13 @@
 function cgh(id){
     let button = document.getElementById("img_1");
-    if(button.src.indexOf("/images/icon%20menu.png")>0){
-        button.src='/images/IsActive_menu.png';
+    if(button.src.indexOf("/images/icon%20menu.png")>0) {
+        button.src = '/images/IsActive_menu.png';
+        let elem = document.getElementById("nav-list");
+        elem.style.display = 'block';
     }
     else{
         button.src='/images/icon%20menu.png';
+        let elem = document.getElementById("nav-list");
+        elem.style.display = 'none';
     }
 }
